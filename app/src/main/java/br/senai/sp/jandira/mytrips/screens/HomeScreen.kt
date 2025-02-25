1de559d358b0fc2319549154f3abd0fa0f2628a3
NICOLAS SILVA DE ALMEIDA SANTOS <24122316@dsn127m1246729.cfp127aluno.local>
2025-02-25 16:42:07 -0300

    botoes e input criados

--- app/src/main/java/br/senai/sp/jandira/mytrips/screens/HomeScreen.kt
+++ app/src/main/java/br/senai/sp/jandira/mytrips/screens/HomeScreen.kt
@@ -1,22 +1,30 @@
 package br.senai.sp.jandira.mytrips.screens
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalMapOf
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.senai.sp.jandira.mytrips.R
 
 @Composable
@@ -26,6 +34,7 @@ fun Homescreen(){
             .fillMaxSize()
             .background(Color(0xFF171923))
     ){
+
         Card(
             colors = CardDefaults.cardColors(
                 Color(0xFF1B3A9B)
@@ -34,20 +43,10 @@ fun Homescreen(){
                 .height(40.dp)
                 .width(115.dp)
                 .align(Alignment.TopEnd),
+
             shape = RoundedCornerShape(bottomStart = 15.dp),
 
         ){}
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .align(Alignment.Center)
-        ){
-            Column {
-                Text(
-                    text = stringResource(R.string.login) //esperar um pouquinho se nao o R nao aparece
-                )
-            }
-        }
         Card(
             colors = CardDefaults.cardColors(
                 Color(0xFF1B3A9B)
@@ -59,6 +58,61 @@ fun Homescreen(){
             shape = RoundedCornerShape(topEnd = 15.dp),
 
             ){}
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.Center)
+                .padding(20.dp),
+            verticalArrangement = Arrangement.SpaceBetween
+
+        ){
+            Column {
+                Text(
+                    text = stringResource(R.string.login), //esperar um pouquinho se nao o R nao aparece
+                    fontSize = 40.sp
+                )
+                Text(
+                    text = stringResource(R.string.description), //esperar um pouquinho se nao o R nao aparece
+                    fontSize = 20.sp
+                )
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = {},
+                    label = {
+                        Text(text = stringResource(R.string.placeholder))
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                )
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = {},
+                    label = {
+                        Text(text = stringResource(R.string.password))
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                )
+                Button(
+                    onClick = {},
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color(0xFF1B3A9B)
+                    ),
+                    modifier = Modifier
+                        .align(Alignment.End)
+                ) {
+                    Text(
+                        stringResource(R.string.sign_in)
+                    )
+                }
+                Text(
+                    stringResource()
+                )
+
+            }
+
+        }
+
     }
 }
 
